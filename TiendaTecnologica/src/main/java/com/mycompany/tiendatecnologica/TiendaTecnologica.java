7d5d705feba705185ddbb949693ba9800659a4c3
patriciaaguayo <patriciaaguayop@gmail.com>
2024-11-16 18:17:20 +0100

    Cambios en el código.

--- TiendaTecnologica/src/main/java/com/mycompany/tiendatecnologica/TiendaTecnologica.java
+++ TiendaTecnologica/src/main/java/com/mycompany/tiendatecnologica/TiendaTecnologica.java
@@ -12,5 +12,10 @@ public class TiendaTecnologica {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("prueba");
+        int n = 1;
+        int suma = 0;
+        suma += n+1;
+        System.out.println("Mi numero más uno es " + suma);
     }
 }
